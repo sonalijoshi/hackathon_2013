01d274f906e9b6897d573876bc550bb100aef27e
Parthi Jaganathan <parthi.jaganathan@pearson.com>
2013-06-06 18:26:04 -0400

    adding chart

--- js/main.js
+++ js/main.js
@@ -56,15 +56,15 @@ var hack=(function () {
         var index;
         var html = '<ul>';
         var div_html = "";
-        var ingredients = "&#60ul&#62";
+        //var ingredients = "&#60ul&#62";
 
         $.each(data.results, function(i, val) {
             index = i + 1;
             
-            $.each(val.ingredients,function(i,ingredient){              
-                ingredients += '&#60li&#62'+ingredient+'&#60&#47li&#62';
-            });
-            ingredients += "&#60&#47ul&#62";
+            // $.each(val.ingredients,function(i,ingredient){              
+            //     ingredients += '&#60li&#62'+ingredient+'&#60&#47li&#62';
+            // });
+            // ingredients += "&#60&#47ul&#62";
 
             html += '<li><img style="height: 90px;" src="'+val.thumb+'"/><a href="#" class="nolink" onclick="hack.getRecipieById(this.id)" id='+val.id+'>'+val.name+'</a></li>';
 
@@ -79,8 +79,72 @@ var hack=(function () {
 
 
     var drawRecipieGraph = function(data){
-        drawGraph(data);
+        drawNutritionGraph(data);
     };
+
+
+
+    function drawNutritionGraph(data){
+        var nutrution = data.nutritional_info;
+        console.log(nutrution);
+
+            // Radialize the colors
+        Highcharts.getOptions().colors = Highcharts.map(Highcharts.getOptions().colors, function(color) {
+            return {
+                radialGradient: { cx: 0.5, cy: 0.3, r: 0.7 },
+                stops: [
+                    [0, color],
+                    [1, Highcharts.Color(color).brighten(-0.3).get('rgb')] // darken
+                ]
+            };
+        });
+        
+        // Build the chart
+        $('#graphContainer').highcharts({
+            chart: {
+                plotBackgroundColor: null,
+                plotBorderWidth: null,
+                plotShadow: false
+            },
+            title: {
+                text: ''
+            },
+            tooltip: {
+                pointFormat: '{series.name}: <b>{point.percentage}%</b>',
+                percentageDecimals: 1
+            },
+            plotOptions: {
+                pie: {
+                    allowPointSelect: true,
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: true,
+                        color: '#000000',
+                        connectorColor: '#000000',
+                        formatter: function() {
+                            return '<b>'+ this.point.name +'</b>: '+ this.percentage +' %';
+                        }
+                    }
+                }
+            },
+            series: [{
+                type: 'pie',
+                name: 'Nutrition Information',
+                data: [
+                    ['Calcium',   45.0],
+                    ['Potasium',       26.8],
+                    {
+                        name: 'fat',
+                        y: 12.8,
+                        sliced: true,
+                        selected: true
+                    },
+                    ['Iron',   0.7]
+                ]
+            }]
+        });
+    
+    }
 
 	function drawGraph(data){
 
